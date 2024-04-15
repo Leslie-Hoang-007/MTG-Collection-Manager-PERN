cd155a8b905ec51d1da968ccdd3c5b090cbc6306
Leslie-Hoang-007 <111301770+Leslie-Hoang-007@users.noreply.github.com>
2024-04-15 10:34:10 -0400

    added link to image but need to crop to correct size. correct size in above broken image link

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -24,6 +24,7 @@ export const Home = () => {
             <div className="container">
                 <div className="home-header">
                     <img src="https://mtgproshop.com/wp-content/uploads/2020/10/Jace-Poster-Banner-V2-2880x960-1-scaled.jpg.webp"/>
+                    <img src="https://images.squarespace-cdn.com/content/v1/6117360e2f0be106838fc4e6/dd8d55d6-e913-453b-a0b1-139cf3564981/MTG-25th-Anniversary.jpg"/>
                     <div className="home-block">
                         <h1>Track Your Magic the Gathering card collection</h1>
                         <p>An MTG card database and collection tracker. Create an account to start collecting</p>
