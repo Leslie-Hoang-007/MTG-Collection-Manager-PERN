be6a1dc0d770f8a2bdfefc637ffd124f0829c640
Leslie-Hoang-007 <111301770+Leslie-Hoang-007@users.noreply.github.com>
2024-05-02 16:04:40 -0400

    added homepage image be in front end media rather than an external link

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,6 +1,7 @@
 import { Link, useNavigate, Redirect } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import { useEffect } from "react";
+import homeImage from "../media/homepage.jpg";
 
 export const Home = () => {
     // cookies
@@ -23,8 +24,7 @@ export const Home = () => {
         <main className="main">
             <div className="container">
                 <div className="home-header">
-                    <img src="https://mtgproshop.com/wp-content/uploads/2020/10/Jace-Poster-Banner-V2-2880x960-1-scaled.jpg.webp"/>
-                    <img src="https://images.squarespace-cdn.com/content/v1/6117360e2f0be106838fc4e6/dd8d55d6-e913-453b-a0b1-139cf3564981/MTG-25th-Anniversary.jpg"/>
+                    <img src={homeImage} alt="A wisard like man with a cape looking off into the distance."/>
                     <div className="home-block">
                         <h1>Track Your Magic the Gathering card collection</h1>
                         <p>An MTG card database and collection tracker. Create an account to start collecting</p>
